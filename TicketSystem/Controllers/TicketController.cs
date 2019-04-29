c320b61805e807005140d2ca9817966594aa3f9b
Nicolaj Lyth <nlyth17@student.aau.dk>
2019-04-29 11:45:08 +0200

    Ticketcontroller

---
+++ TicketSystem/Controllers/TicketController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketSystem.Controllers
+{
+    public class TicketController : Controller
+    {
+        public IActionResult Index()
+        {
+            var information = new Models.Ticket()
+            {
+                CustomerName = "Dronning Magrethe",
+                TicketID = 123151,
+            };
+
+            ViewData["Ticket"] = information;
+
+            return View();
+        }
+    }
+}
---
+++ TicketSystem/Models/Ticket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Models
+{
+    public class Ticket
+    {
+        public string CustomerName { get; set; }
+        public int TicketID { get; set; }
+        
+    }
+}
