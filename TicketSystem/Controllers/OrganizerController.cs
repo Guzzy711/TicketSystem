0f7f0e81c1588caa9c4caf0e1c55cc01e9094a14
Nicolaj Lyth <nlyth17@student.aau.dk>
2019-04-30 11:16:58 +0200

    Organizer form stuff ?

--- TicketSystem/Controllers/OrganizerController.cs
+++ TicketSystem/Controllers/OrganizerController.cs
@@ -3,17 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace TicketSystem.Controllers
 {
     public class OrganizerController : Controller
     {
+
+        [HttpGet]   //httpget er dog default value
         public IActionResult Index()
         {
 
 
             return View();
         }
+
+        [HttpPost]                                   //route
+        public IActionResult Index(string name, string password)        //bedre måde at skrive de tpå men fungere ikke:Index(OrganizerModel model
+        {
+            if (ModelState.IsValid)
+            {
+               //InsertLogin(model.Name, model.Password);         //Mangler database
+
+                return RedirectToAction("Index", "Home");           //skal laves om til organizer home
+            }
+
+            return View();
+        }
+
+
+
+        private void InsertLoginInformatin(string name, string password)
+        {
+
+        }
+
+
 
 
         public IActionResult CreateOrganizer()
--- TicketSystem/Models/Organizer.cs
+++ TicketSystem/Models/Organizer.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+
+
 namespace TicketSystem.Models
 {
     public class Organizer
     {
-        public Organizer()
-        {
+        [Required]
+        public string Name { get; set; }
 
-        }
+        [Required]
+        public string Password { get; set; }
+
+        
     }
 }
