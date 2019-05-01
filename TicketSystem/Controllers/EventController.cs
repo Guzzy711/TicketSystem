7def075dabf2813f77c4fb5faf1ff3e2fdd299a0
Bogi <bogiberg@icloud.com>
2019-05-01 14:20:33 +0200

    TableView

--- TicketSystem/Controllers/EventController.cs
+++ TicketSystem/Controllers/EventController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Data; 
+using System.Data;
+using System.Collections.Specialized;
 using Microsoft.AspNetCore.Mvc;
 using TicketSystem.Helpers; 
 
@@ -10,11 +11,34 @@ namespace TicketSystem.Controllers
 {
     public class EventController : Controller
     {
-
+        DBhelper dBhelper = new DBhelper(); 
 
         public IActionResult Index()
         {
+            string columns = "EventName, Location, Description"; 
+            DataTable queryResult = dBhelper.SelectQuery($"SELECT {columns} FROM Events");
 
+            string[,] events = new string[queryResult.Rows.Count,queryResult.Columns.Count];
+
+            int rowCounter = 0;
+            int columnCounter=0;
+
+            foreach (DataRow row in queryResult.Rows)
+            {
+                columnCounter = 0; 
+                foreach (DataColumn column in queryResult.Columns)
+                {
+                    events[rowCounter, columnCounter] = (string)row[column.ColumnName];
+                    columnCounter++;
+                }
+                rowCounter++;
+            }
+
+          
+            ViewBag.rows = rowCounter;
+            ViewBag.cols = columnCounter;
+           
+            ViewBag.events = events;
 
             return View();
         }
