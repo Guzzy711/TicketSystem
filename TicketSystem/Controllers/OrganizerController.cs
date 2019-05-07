ad818866a556841dfc4c7cb5e4a6dfc63317d2cd
Nicolaj Lyth <nlyth17@student.aau.dk>
2019-05-07 11:36:50 +0200

    Landingpage ny

--- TicketSystem/Controllers/OrganizerController.cs
+++ TicketSystem/Controllers/OrganizerController.cs
@@ -68,16 +68,18 @@ namespace TicketSystem.Controllers
 
 
 
-        [HttpPost]                                   //route
-        public IActionResult EditEvent(string name, string location, string date, string time, int ticketamount, int price, string image, string description)        //bedre måde at skrive de tpå men fungere ikke:Index(OrganizerModel model
+        [HttpGet]                                   //route
+        public IActionResult EditEvent(int id, string name, string location, string date, string time, int ticketamount, int price, string image, string description)        //bedre måde at skrive de tpå men fungere ikke:Index(OrganizerModel model
         {
             if (ModelState.IsValid)
             {
 
+                dbhelper.InsertQueryToDB($" UPDATE events SET event_name='Nicolaaaaj', location='Aalborg', ticket_amount=5, price=5, description='description' WHERE id=43;");
+
+            // dbhelper.InsertQueryToDB($"UPDATE events SET event_name='{name}', location='{location}', date='{date}', time='{time}', ticket_amount={ticketamount}, price={price}, image='{image}', description='{description}' WHERE id={id}");
 
 
-                dbhelper.InsertQueryToDB($"UPDATE events (event_name, location, date, time, ticket_amount, price, image, description) VALUES ('{name}','{location}','{date}','{time}','{ticketamount}','{price}','{image}','{description}')");
-                return RedirectToAction("Index", "Event");           //skal laves om til organizer home
+           //skal laves om til organizer home
             }
 
             return View();
