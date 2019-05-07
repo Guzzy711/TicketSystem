bbc322292081f2ec7824a2849d5aa97c88799bdc
Christian <Christian@guzzy.dk>
2019-05-07 09:01:47 +0200

    remove required attributes

--- TicketSystem/Models/Organizer.cs
+++ TicketSystem/Models/Organizer.cs
@@ -10,19 +10,19 @@ namespace TicketSystem.Models
 {
     public class Organizer
     {
-        [Required]
+
         public string ContactPerson { get; set; }
 
-        [Required]
+
         public string Password { get; set; }
 
-        [Required]
+
         public string PhoneNumber { get; set; }
 
-        [Required]
+
         public string Email { get; set; }
 
-        [Required]
+
         public string OrganizationName { get; set; }
 
         public int OrganizerID { get; set; }
