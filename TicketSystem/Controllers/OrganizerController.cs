075ae9f042584440bbe0da2b189d7aeff31b0046
Nicolaj Lyth <nlyth17@student.aau.dk>
2019-05-13 12:07:47 +0200

    deletevent knap

--- TicketSystem/Controllers/OrganizerController.cs
+++ TicketSystem/Controllers/OrganizerController.cs
@@ -116,6 +116,24 @@ namespace TicketSystem.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult DeleteEvent(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                dbhelper.DeleteQuery($"DELETE FROM events WHERE id={id}");
+                //dbhelper.DeleteQuery($"DELETE * FROM organizers WHERE id={id}");
+
+                return RedirectToAction("OrganizerLandingPage","Organizer");        //skal laves om til organizer home
+            }
+
+            return View();
+        }
+        public IActionResult DeleteEvent()
+        {
+            return View();
+        }
+
 
 
         [HttpGet]
@@ -123,9 +141,7 @@ namespace TicketSystem.Controllers
         {
             if (ModelState.IsValid)
             {
-                dbhelper.InsertQueryToDB($"UPDATE tickets SET ticket_used='{1}' WHERE id={id}");
-
-                var eventt = dbhelper.CreateOneEventObject(id);
+                dbhelper.InsertQueryToDB($"UPDATE tickets SET ticket_used='{1}' WHERE id={id}"); 
 
                 return RedirectToAction("CheckTickets", "Organizer");
             }
